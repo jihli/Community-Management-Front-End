bea56bb2108d0cd229d06ce45eaec69907011911
CYX <dangeboe@gmail.com>
2024-11-22 13:59:05 -0500

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Layout } from "antd";
+import SiderMenu from "./components/SiderMenu";
+import Maintenance from "./components/Maintenance";
+import AmenityReservation from "./components/AmenityReservation";
+
+const { Sider, Content } = Layout;
 
 function App() {
+  const [selectedMenu, setSelectedMenu] = useState("maintenance");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible theme="dark" width={250}>
+        <SiderMenu onSelect={setSelectedMenu} />
+      </Sider>
+      <Layout>
+        <Content style={{ padding: "24px" }}>
+          {selectedMenu === "maintenance" && <Maintenance />}
+          {selectedMenu === "amenityReservation" && <AmenityReservation />}
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
